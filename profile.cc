fd896698c635f1fc2dc5e72f90865d01ba639f6c
Daviji <dolbear61@yahoo.com>
2019-05-17 22:17:00 -0700

    Stop assuming goal velocity = initial velocity

--- profile.cc
+++ profile.cc
@@ -5,8 +5,9 @@ const double Profile::GetTime(bool ignore_max_velocity) {
   if (ignore_max_velocity) {
     return std::sqrt((2*goal_.position) / kMaxVelocity);
   } else {
-    int time_accelerating = (kMaxVelocity + current_.velocity) / kMaxAcceleration;
-    int time_constant = ((goal_.position - (time_accelerating * kMaxVelocity)) / kMaxVelocity);
-    return (2 * time_accelerating) + time_constant;
+    double time_accelerating = (kMaxVelocity + current_.velocity) / kMaxAcceleration;
+    double time_decelerating = (kMaxVelocity + goal_.velocity) / kMaxAcceleration;
+    double time_constant = ((goal_.position - (0.5*(time_accelerating * kMaxVelocity) + 0.5*(time_decelerating * kMaxVelocity))) / kMaxVelocity);
+    return time_accelerating + time_constant + time_decelerating;
   }
 }
