256e941082fa969b1e16597b6e7feeb897c5f3b0
Daviji <dolbear61@yahoo.com>
2019-05-22 00:02:24 -0700

    Fix time_accelerating calculation

--- profile.cc
+++ profile.cc
@@ -5,7 +5,7 @@ const double Profile::GetTime(bool ignore_max_velocity) {
   if (ignore_max_velocity) {
     return std::sqrt((2*goal_.position) / kMaxVelocity);
   } else {
-    double time_accelerating = (kMaxVelocity + current_.velocity) / kMaxAcceleration;
+    double time_accelerating = (kMaxVelocity - current_.velocity) / kMaxAcceleration;
     double time_decelerating = (kMaxVelocity + goal_.velocity) / kMaxAcceleration;
     double time_constant = ((goal_.position - (0.5*(time_accelerating * kMaxVelocity) + 0.5*(time_decelerating * kMaxVelocity))) / kMaxVelocity);
     return time_accelerating + time_constant + time_decelerating;
